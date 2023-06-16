a9ad61ba79da854df50676d189ae63e268226ac0
gschaithanya <chaithanyags93@gmail.com>
2023-06-16 09:11:05 +0530

    added content in helloworld file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
       System.out.println("Hello all, Shahataaz and Yogesh");
+      System.out.println("Welcome to the world of devops")   
+
    }
 }
